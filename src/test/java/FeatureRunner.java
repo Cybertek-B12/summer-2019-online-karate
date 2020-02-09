abe68c632695e3e699cea974fd440d9aac67bf20
marufjont <marufjont@gmail.com>
2020-02-09 17:23:36 -0500

    initial commit

---
+++ src/test/java/FeatureRunner.java
@@ -0,0 +1,11 @@
+import com.intuit.karate.junit5.Karate;
+
+class FeatureRunner {
+    
+    @Karate.Test
+    Karate testUsers() {
+//        return new Karate().feature("my.feature").relativeTo(getClass());
+        return new Karate().tags("example").relativeTo(getClass());
+    }
+
+}
